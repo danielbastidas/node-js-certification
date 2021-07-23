3eed294116a907a7120e5104ba2217e8214ca6b9
danielbastidas <dabastidasg@gmail.com>
2021-07-23 16:47:54 +0200

    Adding stream unit tests

--- stream-adventure/tests/meet-pipe.js
+++ stream-adventure/tests/meet-pipe.js
@@ -1,12 +1,27 @@
 const fs = require('fs')
 const { pipeline } = require('stream')
 
-function fileToStream(filePath, writableStream) {
+function fileToStream(filePath, writableStream, callback) {
 
     pipeline(fs.createReadStream(filePath), writableStream, err => {
-        if (err) throw err
+        if (err) callback(err)
+        callback()
     })
 }
 
-module.exports = fileToStream
+function fileToStreamPromise(filePath, writableStream) {
+
+    return new Promise((resolve, reject) => {
+        pipeline(fs.createReadStream(filePath), writableStream, error => {
+            if (error) reject(new Error('PIPELINE_ERROR'))
+            resolve()
+        })
+    })
+
+}
+
+module.exports = {
+    fileToStream,
+    fileToStreamPromise
+}
 
